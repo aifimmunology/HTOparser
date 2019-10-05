7d7a5fac01b4a3e3181d0a8df3a499afedee531a
Lucas Graybuck <lucasg@alleninstitute.org>
2019-10-04 17:14:39 -0700

    Adding test skeletons

--- tests/testthat/test-awk_result_parsing.R
+++ tests/testthat/test-awk_result_parsing.R
@@ -1,3 +1,31 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
+context("awk_result_parsing")
+library(HTOparser)
+library(data.table)
+
+# load test data
+test_hto_table <- fread(system.file("testdata/awk_pipeline_counts_table.csv.gz",
+                                    package = "HTOparser"))
+colnames(test_hto_table) <- c("count","cell_barcode","hto_barcode")
+
+test_that(
+  "fuzzy_filtering removes non-matching hto barcodes",
+  {
+
+  }
+)
+
+
+test_that(
+  "fuzzy_filtering retains near-matching barcodes",
+  {
+
+  }
+)
+
+
+test_that(
+  "barcode_table_to_matrix converts a barcode table to a dgCMatrix",
+  {
+
+  }
+)
--- tests/testthat/test-hash_matrix_analysis.R
+++ tests/testthat/test-hash_matrix_analysis.R
@@ -1,3 +1,55 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
+context("hash_matrix_analysis")
+library(HTOparser)
+library(data.table)
+
+# load test data
+test_hto_table <- fread(system.file("testdata/CITE-seq_count_matrix.csv.gz",
+                                    package = "HTOparser"))
+test_hto_mat <- as(as.matrix(test_hto_table[,-1]), "dgCMatrix")
+
+test_hto_key <- fread(system.file("testdata/CITE-seq_count_tags.csv",
+                                  package = "HTOparser"),
+                      header = FALSE)
+
+rownames(test_hto_mat) <- test_hto_key$V1[match(test_hto_table[[1]], test_hto_key$V2)]
+
+test_that(
+  "select_hash_cutoff selects a cutoff to split HTO values",
+  {
+
+  }
+)
+
+
+test_that(
+  "binarize_hash converts a vector of values to binary values based on a cutoff",
+  {
+
+  }
+)
+
+
+test_that(
+  "binarize_hash_matrix converts a matrix of values to a binary matrix with automatic cutoffs",
+  {
+
+  }
+)
+
+
+test_that(
+  "binarize_hash_matrix converts a matrix of values to a binary matrix with manual cutoffs",
+  {
+
+  }
+)
+
+
+test_that(
+  "categorize_binary_hash_matrix converts a binary matrix to categorical results",
+  {
+
+  }
+)
+
+
