84b09f65e3cca7fba79963da601b94310d814646
Lucas Graybuck <lucasg@alleninstitute.org>
2020-02-27 16:31:34 -0800

    version and doc updates

--- R/hash_matrix_analysis.R
+++ R/hash_matrix_analysis.R
@@ -1,9 +1,9 @@
 #' Select a cutoff for hash counts using automatic, k-means based splitting
 #'
 #' @param x a numeric vector of hash count values
-#' @param use_median_cut a logical value indicating whether or not to use the median value of x to set a minimum threshold.
+#' @param use_median_cut a logical value indicating whether or not to use the median value of x to set a minimum threshold. Default is FALSE.
 #' @param min_cut a numeric value for the minimum number of counts to consider. Default is 10.
-#' @param min_fc a numeric value for the minimum expected fold change between the centers of the two clusters (positive and negative). Default is 4.
+#' @param min_fc a numeric value for the minimum expected fold change between the centers of the two clusters (positive and negative). Default is 2.
 #' @param seed a value to use as a random seed for k-means clustering. Default is 3030.
 #'
 #' @return a numeric value to use as a cutoff. If no values are above min_cut, returns max(x), otherwise returns max(x) for non-passing values.
